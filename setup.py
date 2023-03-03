2acb27003167afd77768c899a91e91c0836680af
wenig <phillip.wenig@hpi.de>
2023-03-03 17:48:32 +0100

    fix: references

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="time2feat",
     version="0.1.0",
-    packages=find_packages(include=["t2f"]),
+    packages=find_packages(include=["t2f", "t2f.*"]),
     python_requires=">=3.8",
 )
